018df8771572ce98aea91f927dc63a934aaa81d7
preitychib <preeti.chib2015@gmail.com>
2022-07-17 01:33:43 +0530

    Added field of course model without relations

--- course/models.py
+++ course/models.py
@@ -1,3 +1,29 @@
+from django.utils import timezone
 from django.db import models
 
-# Create your models here.
+
+class CourseModel(models.Model):
+    """Model definition for CourseModel."""
+    COURSE_STATUS = (
+        ('approved', 'approved'),
+        ('not-approved', 'not-approved'),
+        ('drafted', 'drafted'),
+    )
+
+    course_name = models.TextField()
+    created_on = models.DateTimeField(default=timezone.now)
+
+    published_on = models.DateTimeField()
+    course_status = models.CharField(choices=COURSE_STATUS, max_length=14)
+
+    # Todo: Add relation fields
+
+    class Meta:
+        """Meta definition for CourseModel."""
+
+        verbose_name = 'Course'
+        verbose_name_plural = 'Courses'
+
+    def __str__(self):
+        """Unicode representation of CourseModel."""
+        return self.course_name
