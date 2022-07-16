81f12577e20e979405a1f13fe3fa80a1fed5d9e2
preitychib <preeti.chib2015@gmail.com>
2022-07-17 01:48:55 +0530

    Added Section and video model fields without relational fields

--- course_section/models.py
+++ course_section/models.py
@@ -1,3 +1,21 @@
 from django.db import models
 
-# Create your models here.
+
+class CourseSectionModel(models.Model):
+    """Model definition for CourseSectionModel."""
+    section_tittle = models.TextField()
+    
+    # description = models.TextField() / json?
+    # course=models 1:n
+
+    # TODO: Define realion fields here
+
+    class Meta:
+        """Meta definition for CourseSectionModel."""
+
+        verbose_name = 'Section'
+        verbose_name_plural = 'Sections'
+
+    def __str__(self):
+        """Unicode representation of CourseSectionModel."""
+        return self.section_tittle
--- course_video/models.py
+++ course_video/models.py
@@ -1,3 +1,23 @@
 from django.db import models
 
-# Create your models here.
+
+class CourseVideo(models.Model):
+    """Model definition for CourseVideo."""
+
+    video_title = models.TextField()
+    #description=models.TextField()
+    # section= 1:n
+    video_link = models.URLField(blank=True, null=True)
+    video_link_public_id = models.TextField(blank=True, null=True)
+
+    # TODO: Define fields here
+
+    class Meta:
+        """Meta definition for CourseVideo."""
+
+        verbose_name = 'CourseVideo'
+        verbose_name_plural = 'CourseVideos'
+
+    def __str__(self):
+        """Unicode representation of CourseVideo."""
+        pass
